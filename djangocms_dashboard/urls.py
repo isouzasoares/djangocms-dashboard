461de738ae950097a0736f0618d425076f4cd0f0
Leandro Felippo <leandro_pxe@yahoo.com.br>
2018-01-04 14:05:27 -0200

    TKT7214- retirando decorators.

--- djangocms_dashboard/urls.py
+++ djangocms_dashboard/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from decorate_url import decorated_url
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', 'djangocms_dashboard.views.home', name='home'),
-    decorated_url(r'^plugins/$', 'djangocms_dashboard.views.plugins_list', name='plugins_list', wrap=login_required),
-    decorated_url(r'^apphooks/$', 'djangocms_dashboard.views.apphooks_list', name='apphooks_list', wrap=login_required),
+    url(r'^plugins/$', 'djangocms_dashboard.views.plugins_list', name='plugins_list'),
+    url(r'^apphooks/$', 'djangocms_dashboard.views.apphooks_list', name='apphooks_list'),
     # url(r'^$', plugins_list),
-    decorated_url(r'^plugin_detail/(?P<plugin_type>\w+)$', 'djangocms_dashboard.views.plugin_detail', name='plugin_detail', wrap=login_required),
-    decorated_url(r'^apphook_detail/(?P<apphook_class>\w+)$', 'djangocms_dashboard.views.apphook_detail', name='apphook_detail', wrap=login_required)
+    url(r'^plugin_detail/(?P<plugin_type>\w+)$', 'djangocms_dashboard.views.plugin_detail', name='plugin_detail'),
+    url(r'^apphook_detail/(?P<apphook_class>\w+)$', 'djangocms_dashboard.views.apphook_detail', name='apphook_detail')
 ]
